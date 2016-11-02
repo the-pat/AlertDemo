193a48290c6792c25ac85b5e712fe9f54c027761
Patrick Tone <tone@thepat.xyz>
2016-11-02 11:40:45 -0500

    Updated the nuget packages.

--- AlertDemo/Services/SmsService.cs
+++ AlertDemo/Services/SmsService.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
+using Microsoft.AspNet.Identity;
 
 namespace AlertDemo.Services
 {
-    public class SmsService
+    public class SmsService : IIdentityMessageService
     {
+        private readonly TwilioRestClient _service;
+        public Task SendAsync(IdentityMessage message)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
